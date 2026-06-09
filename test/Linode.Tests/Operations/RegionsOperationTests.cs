0e29f792f2c0dca7732942ad57016880345f759d
Andrei Nicholson <contact@andreinicholson.com>
2026-06-08 21:30:15 -0400

    Fix incomplete test

--- test/Linode.Tests/Operations/RegionsOperationTests.cs
+++ test/Linode.Tests/Operations/RegionsOperationTests.cs
@@ -153,22 +153,34 @@ public class RegionsOperationTests
             {
               "data": [
                 {
-                  "axfr_ips": [],
-                  "description": null,
-                  "domain": "example.com",
-                  "expire_sec": 400,
-                  "id": 5678,
-                  "master_ips": [],
-                  "refresh_sec": 401,
-                  "retry_sec": 402,
-                  "soa_email": "admin@example.com",
-                  "status": "active",
-                  "tags": [
-                    "a tag",
-                    "another example"
+                  "capabilities": [
+                    "Linodes",
+                    "Block Storage Encryption"
                   ],
-                  "ttl_sec": 403,
-                  "type": "master"
+                  "country": "us",
+                  "id": "us-west",
+                  "label": "Fremont, CA",
+                  "monitors": {
+                    "alerts": [
+                      "Managed Databases",
+                      "NodeBalancers"
+                    ],
+                    "metrics": [
+                      "Managed Databases",
+                      "NodeBalancers"
+                    ]
+                  },
+                  "placement_group_limits": {
+                    "maximum_linodes_per_flexible_pg": 5,
+                    "maximum_linodes_per_pg": 5,
+                    "maximum_pgs_per_customer": null
+                  },
+                  "resolvers": {
+                    "ipv4": "173.230.145.5, 173.230.147.5, 173.230.155.5, 173.255.212.5, 173.255.219.5, 173.255.241.5, 173.255.243.5, 173.255.244.5, 74.207.241.5, 74.207.242.5",
+                    "ipv6": "2600:3c01::2, 2600:3c01::9, 2600:3c01::5, 2600:3c01::7, 2600:3c01::3, 2600:3c01::8, 2600:3c01::4, 2600:3c01::b, 2600:3c01::c, 2600:3c01::6"
+                  },
+                  "site_type": "core",
+                  "status": "ok"
                 }
               ],
               "page": 2,
@@ -180,18 +192,44 @@ public class RegionsOperationTests
 
         var expected2 = _defaultRegion with
         {
-            DomainName = "example.com",
-            ExpireSec = 400,
-            Id = 5678,
-            RefreshSec = 401,
-            RetrySec = 402,
-            SoaEmail = "admin@example.com",
-            Tags = ["a tag", "another example"],
-            TtlExp = 403
+            Capabilities =
+            [
+                "Linodes",
+                "Block Storage Encryption"
+            ],
+            Country = "us",
+            Id = "us-west",
+            Label = "Fremont, CA",
+            Monitors = new Monitors
+            {
+                Alerts =
+                [
+                    "Managed Databases",
+                    "NodeBalancers"
+                ],
+                Metrics =
+                [
+                    "Managed Databases",
+                    "NodeBalancers"
+                ]
+            },
+            PlacementGroupLimits = new PlacementGroupLimits
+            {
+                MaximumLinodesPerFlexiblePage = 5,
+                MaximumLinodesPerPage = 5,
+                MaximumPagesPerCustomer = null
+            },
+            Resolvers = new Resolvers
+            {
+                Ipv4 = "173.230.145.5, 173.230.147.5, 173.230.155.5, 173.255.212.5, 173.255.219.5, 173.255.241.5, 173.255.243.5, 173.255.244.5, 74.207.241.5, 74.207.242.5",
+                Ipv6 = "2600:3c01::2, 2600:3c01::9, 2600:3c01::5, 2600:3c01::7, 2600:3c01::3, 2600:3c01::8, 2600:3c01::4, 2600:3c01::b, 2600:3c01::c, 2600:3c01::6"
+            },
+            SiteType = SiteType.Core,
+            Status = RegionStatus.Ok
         };
 
         using var container = new OperationContainer();
-        var operation = container.Create<DomainsOperation>(jsonResponses);
+        var operation = container.Create<RegionsOperation>(jsonResponses);
         var response = await operation.List(TestContext.Current.CancellationToken);
 
         Assert.True(response.Successful);
